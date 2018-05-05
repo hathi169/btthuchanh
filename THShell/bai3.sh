6f737000162669db599df1fe4ac51c26a2551cc3
Ha Thi <hathymoon169@gmail.com>
2018-05-05 09:56:56 +0700

    Thuc hanh lap trinh shell
    Thuc hanh lap trinh shell

---
+++ THShell/bai3.sh
@@ -0,0 +1,8 @@
+read -p "Nhap ten thu muc: " tenthumuc
+mkdir $tenthumuc
+if test $? -eq 0;
+then
+	echo "Thu muc $tenthumuc da duoc tao"
+else
+	echo "Khong the tao thu muc $tenthumuc "
+fi
---
+++ THShell/bai4.sh
@@ -0,0 +1,7 @@
+$1 $2
+if test $? -eq 0;
+then 
+	echo "Thanh cong"
+else
+	echo "Error"
+fi
